f7733eb57c58920f7c332122fcc91c65c4b8395f
Stoyan Stoitsev <stoyan.stoitsev@gmail.com>
2023-11-17 12:53:18 +0100

    Initial release

--- nodes/auth/msdynamics-auth.js
+++ nodes/auth/msdynamics-auth.js
@@ -1,74 +1,107 @@
-module.exports = function(RED) {
-    function MSDynamicsAuthNode(config) {
-        RED.nodes.createNode(this, config);
+const querystring = require("querystring");
+let fetch;
 
-        var node = this;
+module.exports = function (RED) {
+  if (!fetch) {
+    fetch = import("node-fetch").then((mod) => mod.default);
+  }
+  function MSDynamicsAuthNode(config) {
+    RED.nodes.createNode(this, config);
 
-        // Retrieve configuration from the node's settings
-        var clientId = config.clientId;
-        var clientSecret = this.credentials.clientSecret;
-        var tenantId = config.tenantId;
-        var instanceUrl = config.instanceUrl;
-        
-        // Function to refresh the token
-        function refreshToken() {
-            const tokenEndpoint = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`;
-            const scope = `${instanceUrl}.default`;
-            const host = "login.microsoftonline.com";
+    const node = this;
+    let refreshTimeout;
 
-            const payload = {
-                grant_type: "client_credentials",
-                client_id: clientId,
-                client_secret: clientSecret,
-                scope: scope
-            };
+    // Initial status
+    node.status({ fill: "yellow", shape: "ring", text: "initializing" });
 
-            const requestOptions = {
-                method: "POST",
-                url: tokenEndpoint,
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Host": host
-                },
-                form: payload
-            };
+    const clientId = config.clientId;
+    const clientSecret = this.credentials.clientSecret;
+    const tenantId = config.tenantId;
+    const instanceUrl = config.instanceUrl;
+    const autoRefresh = config.autoRefresh;
+    const refreshInterval = (config.refreshInterval || 30) * 60 * 1000;
+    const retryInterval = (config.retryInterval || 15) * 60 * 1000;
 
-            RED.util.httpRequest(requestOptions, function(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    const tokenResponse = JSON.parse(body);
-                    const expiresInMilliseconds = tokenResponse.expires_in * 1000;
-                    const expiresAt = Date.now() + expiresInMilliseconds;
+    async function refreshToken() {
+      const fetchModule = await fetch;
+      node.warn("Starting refreshToken function");
 
-                    const tokenData = {
-                        accessToken: tokenResponse.access_token,
-                        expiresAt: expiresAt
-                    };
+      const tokenEndpoint = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`;
+      const payload = {
+        grant_type: "client_credentials",
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: `${instanceUrl}.default`,
+      };
 
-                    // Save token data to global context
-                    node.context().global.set("msDynamicsToken", tokenData);
+      fetchModule(tokenEndpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Host: "login.microsoftonline.com",
+        },
+        body: querystring.stringify(payload),
+      })
+        .then((response) =>
+          response.ok
+            ? response.json()
+            : Promise.reject(
+                new Error(`HTTP error! status: ${response.status}`)
+              )
+        )
+        .then((data) => {
+          const expiresInMilliseconds = data.expires_in * 1000;
+          const expiresAt = Date.now() + expiresInMilliseconds;
 
-                    // Schedule the next refresh
-                    setTimeout(refreshToken, expiresInMilliseconds - (10 * 60 * 1000)); // refresh 10 minutes before expiry
-                } else {
-                    node.error("Failed to refresh MS Dynamics token: " + error);
-                    // Retry after a delay
-                    setTimeout(refreshToken, 15 * 60 * 1000); // retry in 15 minutes
-                }
-            });
-        }
+          const tokenData = {
+            accessToken: data.access_token,
+            expiresAt: expiresAt,
+          };
 
-        // Initial token refresh
-        refreshToken();
+          node.context().global.set("msDynamicsToken", tokenData);
+          node.send([{ payload: data }, null]);
 
-        // In case the user wants to manually trigger a refresh
-        node.on('input', function(msg) {
-            refreshToken();
+          if (autoRefresh) {
+            clearTimeout(refreshTimeout);
+            refreshTimeout = setTimeout(refreshToken, refreshInterval);
+          }
+        })
+        .catch((error) => {
+          node.error("Failed to refresh MS Dynamics token: " + error.message);
+          node.status({ fill: "red", shape: "ring", text: "refresh failed" });
+          node.send([null, { payload: error.message }]);
+
+          if (autoRefresh) {
+            clearTimeout(refreshTimeout);
+            refreshTimeout = setTimeout(refreshToken, retryInterval);
+          }
         });
     }
+    // Validate configuration
+    if (!clientId || !clientSecret || !tenantId || !instanceUrl) {
+      node.error("Missing configuration data");
+      node.status({ fill: "red", shape: "ring", text: "not configured" });
+      return;
+    }
+    // Configuration is valid
+    node.status({ fill: "green", shape: "dot", text: "configured" });
 
-    RED.nodes.registerType("msdynamics-auth", MSDynamicsAuthNode, {
-        credentials: {
-            clientSecret: { type: "text" }
-        }
+    if (autoRefresh) {
+      refreshToken();
+    }
+
+    node.on("input", () => refreshToken());
+
+    node.on("close", () => {
+      if (refreshTimeout) {
+        clearTimeout(refreshTimeout);
+      }
     });
-}
+  }
+
+  RED.nodes.registerType("msdynamics-auth", MSDynamicsAuthNode, {
+    credentials: {
+      clientSecret: { type: "text" },
+    },
+  });
+};
